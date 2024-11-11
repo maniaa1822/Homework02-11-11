268fcd746cd8929ffcbbc5f80d9ba566ee5b74ce
maniaa1822 <manias1822363@studenti.uniroma1.it>
2024-11-11 12:58:16 +0100

    first commit

---
+++ libs/math.py
@@ -0,0 +1,44 @@
+import numpy as np
+
+
+def sigmoid(x):
+    """
+    Function to compute the sigmoid of a given input x.
+
+    Args:
+        x: it's the input data matrix.
+
+    Returns:
+        g: The sigmoid of the input x
+    """
+    ##############################
+    ###     YOUR CODE HERE     ###
+    ##############################  
+    
+    g = 1 / (1 + np.exp(-x))
+    
+      
+    return g
+
+def softmax(y):
+    """
+    Function to compute associated probability for each sample and each class.
+
+    Args:
+        y: the predicted 
+
+    Returns:
+        softmax_scores: it's the matrix containing probability for each sample and each class. The shape is (N, K)
+    """
+    ##############################
+    ###     YOUR CODE HERE     ###
+    ##############################
+    
+    shifted_logits = y - np.max(y, axis=1, keepdims=True)
+    
+    exp_y = np.exp(shifted_logits)
+    
+    softmax_scores = exp_y / np.sum(exp_y, axis=1, keepdims=True)
+    
+    return softmax_scores
+
